eeb0ca55db95d2cc203b650eb76f5445f1edd2a6
Obed Gonzalez <obedgm33@gmail.com>
2020-07-07 02:08:12 -0500

    #12 - code refactor, file deletion, add dependencies

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import { AppProvider } from './store/index'
+import "./index.scss";
+import { ModalProvider } from "react-modal-hook";
+import { TransitionGroup } from "react-transition-group";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <ModalProvider rootComponent={TransitionGroup}>
+        <App />
+      </ModalProvider>
+    </AppProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
